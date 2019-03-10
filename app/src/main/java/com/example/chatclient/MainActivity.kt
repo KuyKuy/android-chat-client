eef844ecb2b78f6b21246a703e70fb87a01118b6
Joan <joan.almeida@redb.ee>
2019-03-10 16:03:53 -0300

    WIP Main activity layout

--- app/src/main/java/com/example/chatclient/MainActivity.kt
+++ app/src/main/java/com/example/chatclient/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.chatclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nickname = findViewById<View>(R.id.nickname) as TextView
+        val btn = findViewById<View>(R.id.enterchat) as Button
+        btn.setOnClickListener({
+            v -> Toast.makeText(this, "Hola " + nickname.text, Toast.LENGTH_LONG).show()
+        })
     }
 }
