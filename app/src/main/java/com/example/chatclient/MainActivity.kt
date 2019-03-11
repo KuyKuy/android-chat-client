678bcf7fbed09fad3f57cc13fa488f6310eea8e8
Joan <joan.almeida@redb.ee>
2019-03-10 22:10:44 -0300

    Add enterChatRoom Activity

--- app/src/main/java/com/example/chatclient/MainActivity.kt
+++ app/src/main/java/com/example/chatclient/MainActivity.kt
@@ -1,22 +1,29 @@
 package com.example.chatclient
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.EditText
+
+const val USER_NICKNAME = "USER_NICKNAME"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val nickname = findViewById<View>(R.id.nickname) as TextView
-        val btn = findViewById<View>(R.id.enterchat) as Button
-        btn.setOnClickListener({
-            v -> Toast.makeText(this, "Hola " + nickname.text, Toast.LENGTH_LONG).show()
-        })
     }
+
+    /** Se llama cuando el usuario hace click en el boton 'enterchat' */
+    fun enterChatRoom(view: View) {
+        val nickEditor = findViewById<EditText>(R.id.nickname)
+        val nickname = nickEditor.text.toString()
+        val intent = Intent(this, ChatRoom::class.java).apply {
+            putExtra(USER_NICKNAME, nickname)
+        }
+
+        startActivity(intent)
+    }
+
 }
